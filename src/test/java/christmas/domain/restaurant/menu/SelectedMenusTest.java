337725f226778d2faa37e4e08fc4719e5fa92cbd
HiiWee <wpdlzhf159@gmail.com>
2023-11-12 18:57:50 +0900

    test: 선택한 모든 메뉴 객체에 대한 단위 테스트 추가

--- src/test/java/christmas/domain/restaurant/menu/SelectedMenusTest.java
+++ src/test/java/christmas/domain/restaurant/menu/SelectedMenusTest.java
@@ -1,11 +1,13 @@
 package christmas.domain.restaurant.menu;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import christmas.validator.domain.exception.DomainExceptionMessage;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -42,5 +44,32 @@ class SelectedMenusTest {
         // then
         assertThatIllegalArgumentException().isThrownBy(() -> SelectedMenus.createFrom(menus))
                 .withMessageContaining(DomainExceptionMessage.INVALID_ORDER.message());
+    }
+
+    @DisplayName("포함되어 있는 모든 메뉴의 타입을 중복 없이 가져올 수 있다.")
+    @Test
+    void extractMenuTypes() {
+        // given
+        SelectedMenus selectedMenus = SelectedMenus.createFrom(List.of("초코케이크-1", "샴페인-10", "바비큐립-5"));
+
+        // when
+        List<MenuType> menuTypes = selectedMenus.extractMenuTypes();
+
+        // then
+        assertThat(menuTypes).contains(MenuType.DESSERT, MenuType.BEVERAGE, MenuType.MAIN);
+    }
+
+    @DisplayName("선택한 메뉴들의 총 가격을 계산할 수 있다.")
+    @Test
+    void calculateTotalPrice() {
+        // given
+        int expectedTotalPrice = Menu.CHOCO_CAKE.price() + Menu.CHAMPAGNE.price() * 10 + Menu.BARBECUE_RIBS.price() * 5;
+        SelectedMenus selectedMenus = SelectedMenus.createFrom(List.of("초코케이크-1", "샴페인-10", "바비큐립-5"));
+
+        // when
+        int totalPrice = selectedMenus.calculateTotalPrice();
+
+        // then
+        assertThat(totalPrice).isEqualTo(expectedTotalPrice);
     }
 }
