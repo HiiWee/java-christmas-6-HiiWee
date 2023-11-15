3139b922b865e07c7c7c8d338d317131e8460e5b
HiiWee <wpdlzhf159@gmail.com>
2023-11-16 05:17:29 +0900

    refactor: 날짜를 LocalDate 활용하여 분리

--- src/main/java/christmas/domain/restaurant/date/DateType.java
+++ src/main/java/christmas/domain/restaurant/date/DateType.java
@@ -1,21 +1,21 @@
 package christmas.domain.restaurant.date;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public enum DateType {
 
-    CHRISTMAS(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)),
-    WEEKDAY(List.of(3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31)),
-    WEEKEND(List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30)),
-    SPECIAL(List.of(3, 10, 17, 24, 25, 31)),
-    NONE(Collections.emptyList());
+    CHRISTMAS(ChristmasPromotionDate::isInChristmas),
+    WEEKDAY(ChristmasPromotionDate::isWeekday),
+    WEEKEND(ChristmasPromotionDate::isWeekend),
+    SPECIAL(ChristmasPromotionDate::isSpecial),
+    NONE(ChristmasPromotionDate::getNone);
 
-    private final List<Integer> dates;
+    private final Predicate<Integer> dateChecker;
 
-    DateType(final List<Integer> dates) {
-        this.dates = dates;
+    DateType(final Predicate<Integer> dateChecker) {
+        this.dateChecker = dateChecker;
     }
 
     public static List<DateType> findDatTypes(final int date) {
@@ -25,6 +25,6 @@ public enum DateType {
     }
 
     private boolean containsDate(final int date) {
-        return dates.contains(date);
+        return dateChecker.test(date);
     }
 }
